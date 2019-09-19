40983e0f6d86fff0206439f69160ba4883ca6319
Esteban Alvarado <estalvgs1999@gmail.com>
2019-09-19 14:41:01 -0600

    [add] cJSON parser,can parse a given 24x16 int matrix

--- Client/src/json/jsonSerializer.c
+++ Client/src/json/jsonSerializer.c
@@ -1,4 +1,44 @@
-//
-// Created by esteban on 18/09/19.
-//
+#include "jsonSerializer.h"
 
+// Function that receives an array and returns a json that contains it as an item
+char* serialize_Matrix(int matrix[ROWS][COLS]){
+
+
+    char *json_File;
+
+    cJSON *_row = cJSON_CreateArray();
+
+    cJSON *json_Object = cJSON_CreateObject();
+    if(json_Object == NULL)
+        goto end;
+
+    cJSON *_matrix = cJSON_CreateArray();
+    if(_matrix == NULL)
+        goto end;
+
+    // This variable creates cJSON objects to be able to enter them into the Array
+    cJSON *_item;
+
+    for(int i = 0; i < ROWS; i++) {
+
+        _row = cJSON_CreateArray();
+        for (int j = 0; j < COLS; j++) {
+            _item = cJSON_CreateNumber(matrix[i][j]);
+            cJSON_AddItemToArray(_row, _item);
+        }
+
+        cJSON_AddItemToArray(_matrix,_row);
+    }
+
+    cJSON_AddItemToObject(json_Object,"objMatrix",_matrix);
+
+    json_File = cJSON_Print(json_Object);
+    if (json_File == NULL)
+    {
+        fprintf(stderr, "Failed to print monitor.\n");
+    }
+
+    end:
+    cJSON_Delete(json_Object);
+    return json_File;
+}
--- Client/src/json/jsonSerializer.h
+++ Client/src/json/jsonSerializer.h
@@ -1,8 +1,19 @@
-//
-// Created by esteban on 18/09/19.
-//
-
 #ifndef CLIENT_JSONSERIALIZER_H
 #define CLIENT_JSONSERIALIZER_H
+
+// Libraries
+#include "../cjson/cJSON.h"
+#include <stdio.h>
+
+#define ROWS 24
+#define COLS 16
+
+/**
+ * @brief Function that receives an array and returns a json that contains it as an item
+ * @param matrix : Matrix for parsing.
+ * @return : Json parsed file.
+ */
+char* serialize_Matrix(int matrix[ROWS][COLS]);
+
 
 #endif //CLIENT_JSONSERIALIZER_H
