19387582d7b7fa1a2d1a2382326a6b9bd37b3089
Esteban Alvarado <estalvgs1999@gmail.com>
2019-09-20 01:33:15 -0600

    [add] All chuz work of this project in one commit made by me

---
+++ Client/src/entity/FruitLogic.h
@@ -0,0 +1,25 @@
+//
+// Created by Jesus on 19/9/2019.
+//
+
+#ifndef KEYDONCE_FRUITLOGIC_H
+#define KEYDONCE_FRUITLOGIC_H
+struct fruit{
+    int iD;
+    int jPos;
+    int iPos;
+    int whichFruit;
+    int onVyne;
+    int onField;
+    int onScreen;
+};
+voidFruitState();
+
+
+
+
+
+
+
+
+#endif //KEYDONCE_FRUITLOGIC_H
